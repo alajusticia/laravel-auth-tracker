a6948315a961a0a0906918f2f4c4cc81c0ca27d8
Anthony Lajusticia <lajusticia.anthony@gmail.com>
2020-02-11 13:25:13 +0100

    Init tests

---
+++ tests/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ALajusticia\AuthTracker\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function testIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
